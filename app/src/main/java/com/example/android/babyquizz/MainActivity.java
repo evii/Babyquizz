591cb9e737cff8544604405927c7f63cc457e5b9
evii <evi.pavelkova@gmail.com>
2017-04-01 17:40:09 +0200

    Dismiss keyboard when click outside of EditText.

--- app/src/main/java/com/example/android/babyquizz/MainActivity.java
+++ app/src/main/java/com/example/android/babyquizz/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.babyquizz;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -20,7 +24,14 @@ import static com.example.android.babyquizz.R.string.result;
 
 public class MainActivity extends AppCompatActivity {
     int finalResult = 0;
+    EditText question5EditText;
 
+    public static void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // setting EditText to 0, so that the app does not crash when the EditText is not filled
-        EditText question5EditText = (EditText) findViewById(R.id.question5);
+        question5EditText = (EditText) findViewById(R.id.question5);
         question5EditText.setText("0");
+
     }
 
     // Method for calculating result
@@ -172,5 +184,25 @@ public class MainActivity extends AppCompatActivity {
         TextView userName = (TextView) findViewById(R.id.User_name_EditText);
         userName.setText(getString(R.string.blank));
 
+    }
+
+    // Dismissing keyboard when click outside of EditText in android
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+
+        if (v != null &&
+                (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + v.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + v.getTop() - scrcoords[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
